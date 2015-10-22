d1aaeb6a452c507c0c9c3b94f596307dbee6f0d8
Xin Suo <xsuo@groupon.com>
2015-10-21 23:44:50 -0700

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Xin Suo on 10/21/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottenTomatoes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  rottenTomatoes
+//
+//  Created by Xin Suo on 10/21/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
